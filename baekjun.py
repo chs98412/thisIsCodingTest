10591caff2051c60c86da22eda702841f9d82918
chs98412 <79582366+chs98412@users.noreply.github.com>
2022-01-16 21:49:46 +0900

    qwe

--- baekjun.py
+++ baekjun.py
@@ -1,26 +1,53 @@
 import sys
-
 n = int(sys.stdin.readline())
-data = sys.stdin.readline().split()
-n = int(sys.stdin.readline())
+data = []
+for i in range(n):
+    data.append(list(sys.stdin.readline().rstrip()))
 
-data2 = sys.stdin.readline().split()
+array = [[0 for col in range(n)] for row in range(n)]
+
+answer = [0, 0]
 
 
-data.sort()
-for i in data2:
-    lst = data
-    l, r = 0, len(data)-1
-    while True:
-        num = (r+l)//2
-        temp = lst[num]
-        if temp == i:
-            print(1)
-            break
-        if r < l:
-            print(0)
-            break
-        if temp > i:
-            r = num-1
-        else:
-            l = num+1
+def dfs2(y, x):
+    array[y][x] = 1
+    if y+1 < n and array[y+1][x] == 0 and (data[y+1][x] == data[y][x] or (data[y+1][x] == "R" and data[y][x] == "G") or (data[y+1][x] == "G" and data[y][x] == "R")):
+        dfs(y+1, x)
+    if x+1 < n and array[y][x+1] == 0 and (data[y][x+1] == data[y][x] or (data[y][x+1] == "R" and data[y][x] == "G") or (data[y][x+1] == "G" and data[y][x] == "R")):
+        dfs(y, x+1)
+
+    if y > 0 and array[y-1][x] == 0 and (data[y-1][x] == data[y][x] or (data[y-1][x] == "R" and data[y][x] == "G") or (data[y-1][x] == "G" and data[y][x] == "R")):
+        dfs(y-1, x)
+    if x > 0 and array[y][x-1] == 0 and (data[y][x-1] == data[y][x] or (data[y][x-1] == "R" and data[y][x] == "G") or (data[y][x-1] == "G" and data[y][x] == "R")):
+        dfs(y, x-1)
+    return 1
+
+
+def dfs(y, x):
+    array[y][x] = 1
+    if y+1 < n and array[y+1][x] == 0 and data[y+1][x] == data[y][x]:
+        dfs(y+1, x)
+    if x+1 < n and array[y][x+1] == 0 and data[y][x+1] == data[y][x]:
+        dfs(y, x+1)
+
+    if y > 0 and array[y-1][x] == 0 and data[y-1][x] == data[y][x]:
+        dfs(y-1, x)
+    if x > 0 and array[y][x-1] == 0 and data[y][x-1] == data[y][x]:
+        dfs(y, x-1)
+    return 1
+
+
+for i in range(len(data)):
+    for j in range(len(data[0])):
+        if array[i][j] == 0:
+            answer[0] += dfs(i, j)
+
+array = [[0 for col in range(n)] for row in range(n)]
+
+for i in range(len(data)):
+    for j in range(len(data[0])):
+        if array[i][j] == 0:
+            answer[1] += dfs2(i, j)
+            print(array)
+
+print(answer[0], answer[1])
