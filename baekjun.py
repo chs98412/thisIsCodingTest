ca1628ec9a9d66899ac1384997f03d483045f005
chs98412 <79582366+chs98412@users.noreply.github.com>
2022-01-18 23:41:46 +0900

    123

--- baekjun.py
+++ baekjun.py
@@ -1,8 +1,55 @@
+from collections import deque
 import sys
-n = int(sys.stdin.readline())
-data = []
-for i in range(n):
-    data.append(int(sys.stdin.readline()))
-data.sort()
-for i in data:
-    print(i)
+
+
+m, n = map(int, sys.stdin.readline().split())
+array = [[0 for col in range(n)] for row in range(m)]
+
+if m+n == 2:
+    a = int(sys.stdin.readline())
+    if a == 1:
+        print("1")
+        print("1")
+    else:
+        print("0")
+        print("0")
+else:
+    data = []
+    for i in range(m):
+        data.append(list(map(int, sys.stdin.readline().split())))
+
+    answer = 0
+    cnt = []
+
+    def bfs(i, j):
+        cnt = 0
+        stk = [[i, j]]
+
+        while len(stk) > 0:
+            temp = stk.pop()
+            y, x = temp[0], temp[1]
+            if array[y][x] == 0:
+
+                cnt += 1
+                array[y][x] = 1
+
+                if x > 0 and data[y][x-1] == 1:
+                    stk.append([y, x-1])
+                if x+1 < n and data[y][x+1]:
+                    stk.append([y, x+1])
+                if y > 0 and data[y-1][x]:
+                    stk.append([y-1, x])
+                if y+1 < m and data[y+1][x]:
+                    stk.append([y+1, x])
+        return cnt
+
+    for i in range(m):
+        for j in range(n):
+            if data[i][j] == 1 and array[i][j] == 0:
+                cnt.append(bfs(i, j))
+    print(len(cnt))
+
+    if len(cnt) == 0:
+        print("0")
+    else:
+        print(max(cnt))
